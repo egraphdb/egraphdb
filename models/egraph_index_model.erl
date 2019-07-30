f00b5d1627fbc9dea324f3d6834aeac60bbcd66f
sujatha.hemmady <sujatha.hemmady@redbus.com>
2019-07-30 13:05:54 +0530

    [Detail API] fix for deleting old index value, to update new index value
    Issue: https://github.com/egraphdb/egraphdb/issues/2

--- models/egraph_index_model.erl
+++ models/egraph_index_model.erl
@@ -31,7 +31,7 @@
 -export([search/6, search/3]).
 -export([read_all_resource/4]).
 -export([create_or_update_info/2]).
--export([delete_resource/3]).
+-export([delete_resource/3, delete_resource/4]).
 -export([read_resource/5]).
 %% -export([sql_insert_record/4]).
 -export_type([egraph_k/0]).
@@ -266,6 +266,36 @@ delete_resource(Key, KeyType, IndexName) ->
     PoolName = egraph_config_util:mysql_rw_pool(index),
     case egraph_sql_util:mysql_write_query(
            PoolName, Query, Params, TimeoutMsec) of
+        ok ->
+            true;
+        _ ->
+            false
+    end.
+delete_resource(Key, KeyType, Value, IndexName) ->
+    DbKey = egraph_shard_util:convert_key_to_datatype(KeyType, Key),
+    BaseTableName = egraph_shard_util:base_table_name(KeyType),
+    {Query, Params} = case KeyType of
+                          <<"geo">> ->
+                              Q = iolist_to_binary([<<"DELETE FROM ">>,
+                                  egraph_shard_util:sharded_tablename(
+                                      IndexName, BaseTableName),
+                                  <<" WHERE key_data=ST_GeomFromGeoJSON(?)">>]),
+                              %% TODO: should we check for collisions?
+                              P = [DbKey],
+                              {Q, P};
+                          _ ->
+                              Q = iolist_to_binary([<<"DELETE FROM ">>,
+                                  egraph_shard_util:sharded_tablename(
+                                      IndexName, BaseTableName),
+                                  <<" WHERE key_data=? and id=?">>]),
+                              %% TODO: should we check for collisions?
+                              P = [DbKey, Value],
+                              {Q, P}
+                      end,
+    TimeoutMsec = egraph_config_util:mysql_rw_timeout_msec(index),
+    PoolName = egraph_config_util:mysql_rw_pool(index),
+    case egraph_sql_util:mysql_write_query(
+        PoolName, Query, Params, TimeoutMsec) of
         ok ->
             true;
         _ ->
